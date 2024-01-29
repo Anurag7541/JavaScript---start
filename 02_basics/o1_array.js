347228651f1307547ad5b58ca0847699247d66b6
Anurag kumar <117309701+Anurag7541@users.noreply.github.com>
2024-01-29 12:14:00 +0000

    Array

---
+++ 02_basics/o1_array.js
@@ -0,0 +1,29 @@
+// array
+
+let myArr = [1,2,3,4,5];
+let myFav = ["Modi jee" , "YogiBaba"];
+const myArr2 = new Array(1,2,3,4,5)
+console.log(myArr[0])
+console.log(myFav[0])
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(9)
+// myArr.shifr()
+
+console.log(myArr.includes(9));
+console.log(myArr);
+
+// slice , splice
+
+console.log("A ",myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+
+console.log("B ",myArr);
