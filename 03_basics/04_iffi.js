7a4bb0e84463bfdbd3fbad97509b8adb1c648446
Anurag kumar <117309701+Anurag7541@users.noreply.github.com>
2024-02-06 11:13:38 +0000

    done

---
+++ 03_basics/04_iffi.js
@@ -0,0 +1,14 @@
+//Immediately Invoked function Expressions (IIFE)
+
+
+(function chai(){
+    ///named iife
+
+    console.log(`DS CONNECTED`)
+})();
+
+// ()()//used global population se bachne ke liye iffe ka use karte hain
+
+( () =>{
+    console.log(`DB CONNECTED TWO`);
+})()
